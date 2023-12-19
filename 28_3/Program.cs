b39b5c075febc63533a479f19808ca539dfa95fb
Federicaaaaaa <danger2006.bona@gmail.com>
2023-12-19 17:07:56 +0100

    Aggiunto la parte non capita

--- 28_3/Program.cs
+++ 28_3/Program.cs
@@ -10,6 +10,14 @@ namespace _28_3
     {
         static void Main(string[] args)
         {
+            /* Per gli immobili è necessario definire un metodo che restituisca, in un formato 
+             * stringa a piacere, il tipo di immobile e tutte le corrispondenti proprietà.
+             * 
+             * L’agenzia deve poter eseguire delle ricerche specificando una chiave. Per le ricerche 
+             * si considera l’operatore di contenimento, cioè si verifica se la chiave di ricerca è 
+             * contenuta all’interno di una proprietà dell’oggetto. Il risultato della ricerca è v
+             * isualizzato a video stampando la scheda dell’immobile corrispondente. 
+             */
         }
     }
 }
